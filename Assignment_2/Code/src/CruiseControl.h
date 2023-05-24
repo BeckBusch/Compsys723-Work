4ea5fbd9447fedd3ba9f0fac60c83bb2c9095863
Beck Busch <beckbusch5@gmail.com>
2023-05-24 14:18:56 +1200

    set up source files

---
+++ Assignment_2/Code/src/CruiseControl.h
@@ -0,0 +1,18 @@
+/** @file CruiseControl.h
+*
+* @brief Cruise control parameters
+*
+*/
+#ifndef CRUISECONTROL_H
+#define CRUISECONTROL_H
+
+#define SpeedMin 30.0           // km/h
+#define SpeedMax 150.0          // km/h
+#define SpeedInc 2.5            // km/h
+#define Kp 8.113
+#define Ki 0.5
+#define ThrottleSatMax 45.0     // percent
+#define PedalsMin 3.0           // percent
+
+#endif /* CRUISECONTROL_H */
+/*** end of file ***/
