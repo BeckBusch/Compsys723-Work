820652fe97521d5290643bcfa8a783a3274159ae
Beck Busch <beckbusch5@gmail.com>
2023-05-30 14:20:56 +1200

    fixed bugs and compiled strl

--- Assignment_2/Code/src/CruiseControl.h
+++ Assignment_2/Code/src/CruiseControl.h
@@ -1,18 +1,8 @@
-/** @file CruiseControl.h
-*
-* @brief Cruise control parameters
-*
-*/
-#ifndef CRUISECONTROL_H
-#define CRUISECONTROL_H
-
-#define SpeedMin 30.0           // km/h
-#define SpeedMax 150.0          // km/h
-#define SpeedInc 2.5            // km/h
+#define SpeedMin 30.0
+#define SpeedMax 150.0
+#define SpeedInc 2.5
 #define Kp 8.113
 #define Ki 0.5
-#define ThrottleSatMax 45.0     // percent
-#define PedalsMin 3.0           // percent
+#define ThrottleSatMax 45.0
+#define PedalsMin 3.0
 
-#endif /* CRUISECONTROL_H */
-/*** end of file ***/
